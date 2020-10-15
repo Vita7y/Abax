bca83971e1c2861f61acde051eac055894bcd42e
Vitaly <vitallium@hotmail.com>
2020-10-15 15:17:14 +0200

    Fix example

--- src/Abax.WebApi/Controllers/StationSelectorController.cs
+++ src/Abax.WebApi/Controllers/StationSelectorController.cs
@@ -1,8 +1,6 @@
 ﻿using Abax.StationSelector;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace Abax.WebApi.Controllers
 {
     [Route("api/[controller]")]
@@ -16,7 +14,7 @@ namespace Abax.WebApi.Controllers
             _stationsStorage = stationsStorage;
         }
 
-        // GET api/<StationSelector>/5
+        // GET api/StationSelector?Station=Aber
         [HttpGet()]
         public StationsSearchResult Get([FromQuery] string station)
         {
